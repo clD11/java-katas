9277a1ed33373b7c2aa14793ef0eb033e54bf941
clD11 <clD11@fake.com>
2017-10-17 12:01:22 +0100

    time conversion challenge

---
+++ test/hackerrank/TimeConversionTest.java
@@ -0,0 +1,16 @@
+package hackerrank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junitparams.JUnitParamsRunner;
+
+@RunWith(JUnitParamsRunner.class)
+public class TimeConversionTest {
+
+	@Test
+	public void testConvertTime() {
+		
+	}
+	
+}
