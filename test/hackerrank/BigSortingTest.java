f074a619003adb90c553ed69e5b1c52ecd2db5e2
clD11 <23483715+clD11@users.noreply.github.com>
2017-10-18 23:11:42 +0100

    big sorting

---
+++ test/hackerrank/BigSortingTest.java
@@ -0,0 +1,30 @@
+package hackerrank;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junitparams.JUnitParamsRunner;
+
+@RunWith(JUnitParamsRunner.class)
+public class BigSortingTest {
+
+	@Test
+	public void testSort() {
+		
+		String a = "31415926535897932384626433832795";
+		String b = "1";
+		String c = "3";
+		String d = "10";
+		String e = "3";
+		String f = "5";
+		
+		String[] arr = new String[] { a, b, c, d, e, f };
+		
+		BigSorting.sort(arr);
+		
+		assertArrayEquals(new String[] { b, c, e, f, d, a }, arr);
+	}
+	
+}
