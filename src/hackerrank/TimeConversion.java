370a4d4f23c40a2ec4c0309e05e9eb3eaf698f1b
clD11 <23483715+clD11@users.noreply.github.com>
2017-10-18 09:51:05 +0100

    time conversion challenge complete

--- src/hackerrank/TimeConversion.java
+++ src/hackerrank/TimeConversion.java
@@ -2,4 +2,19 @@ package hackerrank;
 
 public class TimeConversion {
 
+	public static String convert(String time) {
+		
+		String converted = time.substring(0, 8);		
+		int hour = Integer.parseInt(time.substring(0, 2));
+		
+		if (hour == 12 && time.endsWith("AM")) {			
+			return converted.replaceFirst("12", "00");
+		}
+		
+		if (hour != 12 && time.endsWith("PM")) {			
+			return (hour + 12) + converted.substring(2, 8);
+		}
+		
+		return converted;
+	}
 }
--- test/hackerrank/TimeConversionTest.java
+++ test/hackerrank/TimeConversionTest.java
@@ -1,16 +1,21 @@
 package hackerrank;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
 @RunWith(JUnitParamsRunner.class)
 public class TimeConversionTest {
 
 	@Test
-	public void testConvertTime() {
-		
+	@Parameters({"12:00:00AM,00:00:00", "12:05:05AM,00:05:05", "12:05:05PM,12:05:05",
+				 "07:05:45PM,19:05:45", "07:05:45AM,07:05:45"})
+	public void testConvertTime(String input, String expected) {
+		String actual = TimeConversion.convert(input);
+		assertEquals(expected, actual);
 	}
-	
 }
