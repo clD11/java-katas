3fe0fa7f0f323e6b058cadd55c3b444d7bf401db
kd31 <kit.davies@pinsentmasons.com>
2020-01-12 20:15:13 +0000

    hackerrank test

---
+++ src/hackerrank/java/StdinAndStdoutI.java
@@ -0,0 +1,16 @@
+package hackerrank.java;
+
+import java.util.Scanner;
+
+public class StdinAndStdoutI {
+
+    public void printOutput() {
+        try (Scanner scan = new Scanner(System.in)) {
+            while (scan.hasNext()) {
+                System.out.println(scan.nextInt());
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
