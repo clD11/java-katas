74beec583985d63b6fb4cbdc583afcaaca9cb30b
clD11 <23483715+clD11@users.noreply.github.com>
2020-01-16 17:00:28 +0000

    calculate fraction

---
+++ test/hackerrank/java/CalculateFractionTest.java
@@ -0,0 +1,16 @@
+package hackerrank.java;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculateFractionTest {
+
+    private final CalculateFraction calculateFraction = new CalculateFraction();
+
+    @Test
+    void shouldReturnNaNWhenNoPairTrades() {
+
+    }
+
+}
