6e5adad1640646db2ea74f9e1e25aa9ec8bce76e
clD11 <23483715+clD11@users.noreply.github.com>
2020-01-18 15:55:12 +0000

    grading students

--- test/hackerrank/GradingStudentsTest.java
+++ test/hackerrank/GradingStudentsTest.java
@@ -10,16 +10,20 @@ import static org.hamcrest.Matchers.is;
 
 class GradingStudentsTest {
 
-    GradingStudents gradingStudents = new GradingStudents();
+    private final List<Integer> grades = Lists.newArrayList(4, 73, 67, 38, 33);
+    private final List<Integer> expected = Lists.newArrayList(4, 75, 67, 40, 33);
+
+    private final GradingStudents gradingStudents = new GradingStudents();
 
     @Test
     void shouldGradeStudent() {
-        List<Integer> grades = Lists.newArrayList(4, 73, 67, 38, 33);
-        List<Integer> expected = Lists.newArrayList(4, 75, 67, 40, 33);
-
         List<Integer> actual = gradingStudents.gradingStudents(grades);
-
         assertThat(actual, is(expected));
     }
 
+    @Test
+    void shouldGradeStudentMap() {
+        List<Integer> actual = gradingStudents.gradingStudentsMap(grades);
+        assertThat(actual, is(expected));
+    }
 }
