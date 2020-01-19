86e4f82e8474c51e71e782bcfb310a024617c55a
clD11 <23483715+clD11@users.noreply.github.com>
2020-01-19 14:18:27 +0000

    hackerrank challenges

--- test/hackerrank/problem_solving/KangarooSolution.java
+++ test/hackerrank/problem_solving/KangarooSolution.java
@@ -16,4 +16,23 @@ public class KangarooSolution {
         }
         return "YES";
     }
+
+    public String kangarooRec(int x1, int v1, int x2, int v2) {
+        if (x2 > x1 && v2 > v1) {
+            return "NO";
+        }
+
+        x1 += v1;
+        x2 += v2;
+
+        if (x1 > x2 && v1 >= v2) {
+            return "NO";
+        }
+
+        if (x1 == x2) {
+            return "YES";
+        }
+
+        return kangarooRec(x1, v1, x2, v2);
+    }
 }
