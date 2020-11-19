ea3bd8281491bfc28834e6b498cc82ecfe70a716
clD11 <23483715+clD11@users.noreply.github.com>
2020-11-19 05:51:14 +0000

    Designer PDF Viewer

--- hackerrank/src/main/java/problem_solving/DesignerPDFViewerService.java
+++ hackerrank/src/main/java/problem_solving/DesignerPDFViewerService.java
@@ -1,26 +1,15 @@
 package problem_solving;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class DesignerPDFViewerService {
-
-    public static Map<Character, Integer> charToHeight;
-    static {
-        charToHeight = new HashMap<>();
-        for (int i = 97; i >= 122; i++) {
-            charToHeight.put((char) i, 0);
-        }
-    }
 
     public int designerPdfViewer(int[] h, String word) {
         int tallest = 0;
-        for (int i = 0; i < h.length; i++) {
-            int height = charToHeight.get(h[i]);
+        for (int i = 0; i < word.length(); i++) {
+            int height = h[word.charAt(i) - 97];
             if (height > tallest) {
                 tallest = height;
             }
         }
-        return tallest * h.length;
+        return tallest * word.length();
     }
 }
